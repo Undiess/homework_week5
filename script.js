704f13b48e57ddfa7f9c9a5981db7eba5a653f11
Andy Nguyen <nguyen.andy67@gmail.com>
2020-03-27 16:31:23 +1030

    colour coded time

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
 var date = $("#currentDay");
-var momentDay = moment().format("MMM Do YY"); 
-var currenttime =  moment().format("h"); 
+var momentDay = moment().format("MMM Do YYYY"); 
+var currentHour =  moment().format("HH"); 
+currentHour = 12
 
 date.text(momentDay)
-console.log(currenttime)
+console.log(currentHour)
+
+//change colour if the hour is passed 
+setInterval(function(){
+for(var i=9;i<17;i++){
+    var calenderhour = $("#"+i);
+    if(currentHour>i){
+        calenderhour.css("background-color","yellow")
+    }
+   if(currentHour==i){
+    calenderhour.css("background-color","green")
+   }
+}
+},1000);
+
+//update events 
+ $(".event-col").on("click",function(){
+    $(this).text("hi")
+
+
+ })
