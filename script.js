978f0b413616e519db3e0fa56430bbc637b77d33
Andy Nguyen <nguyen.andy67@gmail.com>
2020-03-27 19:30:13 +1030

    updated text to local storage

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ for(var i=9;i<17;i++){
 }
 },1000);
 
+
 //update events 
  $(".btn").on("click",function(){
     var text = this.parentNode.childNodes[3].value
@@ -27,11 +28,16 @@ for(var i=9;i<17;i++){
 
  var stored = localStorage.getItem("9am");
 
- for (i=9; i<12 ;i++){
+ for (var i=9; i<12 ;i++){
     var stored = localStorage.getItem(i+"am");
-    $("#"+i).placeholder = stored
+    if (stored != null){
+        document.getElementById(i).placeholder = stored;
+        }
 }
-for (i=1; i<12 ;i++){
+for (var i=0; i<4 ;i++){
     var stored = localStorage.getItem(i+"pm");
-    console.log(stored);
+    var num = i+12
+    if (stored != null){
+    document.getElementById(num).placeholder = stored;
+    }
 }
